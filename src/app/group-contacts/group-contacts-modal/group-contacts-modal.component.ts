ceb7fa600804e4d36f5fa70e79d3247d7b8a092c
yitagesB <yitages12@gamil.com>
2019-05-25 22:38:27 -0700

    import contact added

--- src/app/group-contacts/group-contacts-modal/group-contacts-modal.component.ts
+++ src/app/group-contacts/group-contacts-modal/group-contacts-modal.component.ts
@@ -2,12 +2,13 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { UniversityInterface } from '../../register/register';
 import { FormBuilder, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { ELEMENT_DATA } from '../group-contacts.component';
 
-interface ContactsModalInterface {
-    firstname: string;
-    lastname: string;
-    phone: string;
-    university: string;
+interface GroupContactsModalInterface {
+    groupname: string;
+    description: string;
+    // phone: string;
+    // university: string;
 }
 
 export interface DialogData {
@@ -23,12 +24,12 @@ export interface DialogData {
 export class GroupContactsModalComponent implements OnInit {
 
     groupContactsModalForm: any;
-    universities: UniversityInterface[] = [
-        {value: 'Addis Abeba', viewValue: 'Addis Abeba'},
-        {value: 'Adama', viewValue: 'Adama'},
-        {value: 'Bahirdar', viewValue: 'Bahirdar'},
-        {value: 'Hawassa', viewValue: 'Hawassa'}
-    ];
+    // universities: UniversityInterface[] = [
+    //     {value: 'Addis Abeba', viewValue: 'Addis Abeba'},
+    //     {value: 'Adama', viewValue: 'Adama'},
+    //     {value: 'Bahirdar', viewValue: 'Bahirdar'},
+    //     {value: 'Hawassa', viewValue: 'Hawassa'}
+    // ];
     constructor(
         private formBuilder: FormBuilder,
         public dialogRef: MatDialogRef<GroupContactsModalComponent>,
@@ -41,15 +42,16 @@ export class GroupContactsModalComponent implements OnInit {
     ngOnInit(): void {
         // this.getEvent();
         this.groupContactsModalForm = this.formBuilder.group({
-            firstname: [null, [Validators.required]],
-            lastname: [null, [Validators.required]],
-            phone: [null, [Validators.required]],
-            university: [null, [Validators.required]],
+            groupname: [null, [Validators.required]],
+            description: [null, [Validators.required]],
+            // phone: [null, [Validators.required]],
+            // university: [null, [Validators.required]],
         });
     }
 
-    groupContactsModal(groupContactsModalInterface: ContactsModalInterface) {
+    groupContactsModal(groupContactsModalInterface: GroupContactsModalInterface) {
         console.log(groupContactsModalInterface);
+        ELEMENT_DATA.push(groupContactsModalInterface)
     }
 
 }
